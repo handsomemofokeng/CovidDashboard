77b6e241afc79c717bf0f289e7de4ad64d254316
handsome.mofokeng <handsomem23@hotmail.com>
2020-08-17 19:02:31 +0200

    Initial commit

---
+++ app/src/main/java/com/example/coviddashboard/ApplicationClass.java
@@ -0,0 +1,12 @@
+package com.example.coviddashboard;
+
+import android.app.Application;
+
+public class ApplicationClass extends Application {
+
+    public static final String APPLICATION_ID = "2001EE25-28D3-7F3B-FF36-4DFACF69F500";
+    public static final String API_KEY = "FC639AE4-27B9-4A9A-8FF1-F99E9833F2B7";
+    public static final String SERVER_URL = "https://api.backendless.com";
+
+
+}
