30d9850fcaec7418f2552cc48377570d50a7145e
handsome.mofokeng <handsomem23@hotmail.com>
2020-08-17 19:14:00 +0200

    Created AddEntry activity

---
+++ app/src/main/java/com/example/coviddashboard/AddEntry.java
@@ -0,0 +1,14 @@
+package com.example.coviddashboard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddEntry extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_entry);
+    }
+}
