3986223f061133b4622791323f3864a1dd2c3762
handsome.mofokeng <handsomem23@hotmail.com>
2020-08-18 13:46:07 +0200

    added view references on MainActivity

--- app/src/main/java/com/example/coviddashboard/MainActivity.java
+++ app/src/main/java/com/example/coviddashboard/MainActivity.java
@@ -1,24 +1,43 @@
 package com.example.coviddashboard;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.strictmode.IntentReceiverLeakedViolation;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQ_CODE = 1;
+    TextView tvPositiveCases, tvPendingCases;
+    RecyclerView rvEntries;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvPendingCases = findViewById(R.id.tvPending);
+        tvPositiveCases = findViewById(R.id.tvPositive);
+        rvEntries = findViewById(R.id.rvList);
+
     }
 
     public void onClick_EntryAdd(View view) {
-        startActivity(new Intent(getApplicationContext(), AddEntry.class));
+        startActivityForResult(new Intent(getApplicationContext(), AddEntry.class), REQ_CODE);
     }
 
     public void onClick_Refresh(View view) {
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+
     }
 }
